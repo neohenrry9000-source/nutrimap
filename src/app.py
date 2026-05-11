a755dfd45588f2800ef6e9a755d4994dcfafed17
Henrryfx9000 <neohenrry9000@gmail.com>
2026-05-10 22:22:52 -0500

    agregando configuracion de db y añadiendo parte del registro de login y autenticacion

--- src/app.py
+++ src/app.py
@@ -1,30 +1,39 @@
-from src.business.analytics import get_anemia_stats
 from flask import Flask, jsonify
+from src.extensions import db, migrate, jwt
+from src.business.analytics import get_anemia_stats
+from src.customer.routes import customer_bp
 import os
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "NutriMap Activo", 200
+    app.config["SQLALCHEMY_DATABASE_URI"]    = os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"]             = os.environ.get("JWT_SECRET_KEY", "dev-secret")
 
-@app.route('/api/v1/anemia/stats')
-def anemia_stats():
-    stats = get_anemia_stats()
-    return jsonify(stats), 200
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
 
-# Esta es la ruta que te está dando 404
-@app.route('/health')
-def health():
-    return jsonify({"status": "healthy"}), 200
+    app.register_blueprint(customer_bp)
 
+    @app.route("/")
+    def home():
+        return "NutriMap Activo", 200
 
+    @app.route("/api/v1/anemia/stats")
+    def anemia_stats():
+        stats = get_anemia_stats()
+        return jsonify(stats), 200
 
+    @app.route("/health")
+    def health():
+        return jsonify({"status": "healthy"}), 200
 
+    return app
 
+app = create_app()
 
 if __name__ == "__main__":
-    # Render necesita leer la variable PORT
     port = int(os.environ.get("PORT", 8080))
-    # El # nosec evita que el pipeline falle por seguridad
-    app.run(host='0.0.0.0', port=port)  # nosec B104
+    app.run(host="0.0.0.0", port=port)
