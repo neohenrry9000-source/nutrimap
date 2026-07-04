200bdfa3429247e53c3a106dde77ad8e64af3d75
Henrryfx9000 <neohenrry9000@gmail.com>
2026-07-03 22:53:06 -0500

    limpieza de bugs rezagados 1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { AuthProvider } from "./hooks/useAuth.js";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>
 );
